d62ae4656dd3888fcc47f8dfddd4e477d4e87e82
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2025-01-16 14:39:28 +0200

    Solvw 28th problem

---
+++ LeetCode/Easy/28. Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (haystack.length < needle.length) {
+        return -1;
+    }
+
+    for(let i = 0; i <= haystack.length - needle.length; ++i) {
+        if(haystack.substr(i, needle.length) == needle) return i;
+    }
+
+    return -1;
+};
